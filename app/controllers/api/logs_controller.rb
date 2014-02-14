a4215d515ec26d1ec00f97b86d9250ec61332313
Raphael Alampay <raphael.alampay@gmail.com>
2014-02-14 15:06:42 +0800

    User change

--- app/controllers/api/logs_controller.rb
+++ app/controllers/api/logs_controller.rb
@@ -2,22 +2,18 @@ class Api::LogsController < ApplicationController
 
   def logs
     user = User.find(params[:id])
-    if !user
-      User.transaction do
-        params[:logs].each do |l|
-          new_log = Log.new
-          new_log.lon = l[1][:lon]
-          new_log.lat = l[1][:lat]
-          new_log.logged_at = l[1][:logged_at]
-          new_log.user_id = user.id
-          user.logs << new_log
-        end
-        user.save!
+    User.transaction do
+      params[:logs].each do |l|
+        new_log = Log.new
+        new_log.lon = l[1][:lon]
+        new_log.lat = l[1][:lat]
+        new_log.logged_at = l[1][:logged_at]
+        new_log.user_id = user.id
+        user.logs << new_log
       end
-
-      render json: ApiMessage.new(200, "Successfully saved logs", nil)
-    else
-      render json: ApiMessage.new(200, "Error in saving logs", nil)
+      user.save!
     end
+
+    render json: ApiMessage.new(200, "Successfully saved logs", nil)
   end
 end
