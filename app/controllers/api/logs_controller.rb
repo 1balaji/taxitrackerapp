b4be58bebe7c1dbc4009b5a5211ad6a365f8b6e3
Raphael Alampay <raphael.alampay@gmail.com>
2014-02-14 15:50:24 +0800

    fix

--- app/controllers/api/logs_controller.rb
+++ app/controllers/api/logs_controller.rb
@@ -1,6 +1,15 @@
 class Api::LogsController < ApplicationController
 
   def logs
+    user = User.find(params[:id])
+    User.transaction do
+      params[:logs].each do |l|
+        new_log = Log.new
+        new_log.user_id = user.id
+        user.logs << new_log
+      end
+      user.save!
+    end
     render json: ApiMessage.new(200, "Successfully saved logs", nil)
   end
 end
