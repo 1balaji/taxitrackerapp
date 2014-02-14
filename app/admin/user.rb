2c1e23e0041fe13009abe8fcfb4eddf5c3c1eef6
Raphael Alampay <raphael.alampay@gmail.com>
2014-02-14 14:54:25 +0800

    logs

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -10,6 +10,27 @@ ActiveAdmin.register User do
 
   filter :username
 
+  show do |u|
+    attributes_table do
+      row :username
+    end
+    panel "Logs" do
+      columns do
+        u.logs.each do |l|
+          column :lon do
+            l.lon
+          end
+          column :lat do
+            l.lat
+          end
+          column :logged_at do
+            l.logged_at
+          end
+        end 
+      end
+    end
+  end
+
 
   form do |f|
     f.inputs "User Details" do
